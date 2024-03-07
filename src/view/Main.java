03a875692b2b31ab17aeddda6cce23dbdfecd7ed
RENATO <RENATO@DESKTOP-RLBKK5M>
2024-03-07 09:39:34 -0300

    codigo

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,7 @@ public class Main{
 	public static void main(String[]args) {
 		DistroController dist = new DistroController() ;
 		dist.exibeDistro();
-		System.out.println("test windows");
+		System.out.println("		//git@github.com:Heijire/ExerciciosProcessos.git\n"
+				+ " windows");
 	}
 }
